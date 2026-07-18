19e749f25ddadbf35969a9ddbfd3909b0a3a6c7d
paribhoge123 <paribhoge@gmail.com>
2026-07-18 23:44:48 +0530

    Implement twoSum function in solution.cpp

---
+++ 0001-two-sum/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;   // value -> index
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (m.count(complement)) {
+                ans.push_back(m[complement]);
+                ans.push_back(i);
+                return ans;
+            }
+
+            m[nums[i]] = i;
+        }
+
+        return ans;
+    }
+};
